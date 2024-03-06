472b96ec8f6598ba214e545970c1b06dbdf71e40
Chief <brianmwangi10808@gmail.com>
2024-03-06 13:55:22 -0500

    completed

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,20 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
 
+import MesaVerde from "./parks/MesaVerde";
+import RockyMountain from "./parks/RockyMountain";
+
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <>
+      <h1>Colorado State Parks!</h1>
+      <howManyParks />
+      <MesaVerde />
+      <RockyMountain />
+    </>
+  );
 }
+
+export default ColoradoStateParks;
